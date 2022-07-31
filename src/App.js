fd854d246fbfa91f375712efa3dec28457d44334
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-31 16:47:28 +0900

    Organismの作成２ 画像挿入、スタイリング、propsとしてデータを渡す、Cardコンポーネント

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  name: "マイク",
+  image: "https://source.unsplash.com/ZCHj_2lJP00",
+  emial: "aa@gmail.com",
+  phone: "000-000-000",
+  company: {
+    name: "IT Company"
+  },
+  website: "aaa.com"
+};
+
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,18 +1,34 @@
-export const UserCard = () => {
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+
+export const UserCard = (props) => {
+  const { user } = props;
   return (
-    <div>
-      <img alt="プロフィール" />
+    <Card>
+      <img height={200} width={160} src={user.image} alt="プロフィール" />
       <p>名前</p>
-      <dl>
+      <SDl>
         <dt>メール</dt>
-        <dd>aa@gmail.com</dd>
+        <dd>{user.emial}</dd>
         <dt>TEL</dt>
-        <dd>000-000-000</dd>
+        <dd>{user.phone}</dd>
         <dt>会社名</dt>
-        <dd>IT company</dd>
+        <dd>{user.company.name}</dd>
         <dt>WEB</dt>
-        <dd>aaa.com</dd>
-      </dl>
-    </div>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
   );
 };
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+  }
+`;
