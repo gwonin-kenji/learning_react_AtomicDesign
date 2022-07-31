a0fe39aa44f5b80aac70e88e453d4bae0227a55e
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-31 16:12:58 +0900

    Organismの作成１

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>aa@gmail.com</dd>
+        <dt>TEL</dt>
+        <dd>000-000-000</dd>
+        <dt>会社名</dt>
+        <dd>IT company</dd>
+        <dt>WEB</dt>
+        <dd>aaa.com</dd>
+      </dl>
+    </div>
+  );
+};
