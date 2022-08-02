98c4dcf07c3bf9d5776af1beb7a323d9ea1da679
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-08-02 09:23:55 +0900

    ユーザ一覧ページ詳細作成＆スタイリング スタイリング - レスポンシブ(画面の調節に合わせた)なグリッドレイアウトのテンプレートスタイリング

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import { Router } from "./components/router/Router";
+import { Router } from "../src/router/Router";
 import "./styles.css";
-
-const user = {
-  name: "マイク",
-  image: "https://source.unsplash.com/ZCHj_2lJP00",
-  emial: "aa@gmail.com",
-  phone: "000-00r0-000",
-  company: {
-    name: "IT Company"
-  },
-  website: "aaa.com"
-};
 
 export default function App() {
   return <Router />;
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `マイク${val}`,
+    image: "https://source.unsplash.com/ZCHj_2lJP00",
+    emial: "aa@gmail.com",
+    phone: "000-00r0-000",
+    company: {
+      name: "IT Company"
+    },
+    website: "aaa.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザ一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard keys={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  /* レスポンシブ(画面の調節に合わせた)なグリッドレイアウトのテンプレートスタイリング */
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
