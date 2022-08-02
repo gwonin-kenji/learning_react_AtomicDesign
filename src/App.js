2e9d65bc55893fce31772eabf57fa138b6763182
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-08-02 09:03:29 +0900

    Pageの作成１ 全体を routerで置き換え、top, userページを作成

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./components/router/Router";
 import "./styles.css";
 
 const user = {
   name: "マイク",
   image: "https://source.unsplash.com/ZCHj_2lJP00",
   emial: "aa@gmail.com",
-  phone: "000-000-000",
+  phone: "000-00r0-000",
   company: {
     name: "IT Company"
   },
@@ -19,14 +13,5 @@ const user = {
 };
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
